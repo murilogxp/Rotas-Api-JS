0acb28b2d6a89d5b074a3b85750ae6c8e67d935b
murilo <murilosantosgxp@gmail.com>
2023-03-15 18:14:28 -0300

    Configutações do CORS

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import router from "./routes.js";
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(router);
 
 app.listen(port, () => console.log("Servidor Rodando..."));
